9dd8253119f2f0b6b1f057b91b494f06f9a0d60a
Alexander Tkachuk <tksasha@ya.ru>
2014-06-08 11:20:26 +0400

    fix specs

--- spec/controllers/cubes_server_proxy_controller_spec.rb
+++ spec/controllers/cubes_server_proxy_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe CubesController, :type => :controller do
+RSpec.describe CubesServerProxyController, :type => :controller do
 	it { should route(:get, '/cubes').to(action: :index) }
 
 	describe '#index' do
+    before { expect(CubesServerProxy).to receive(:request) }
+
 		before { get :index, format: :json }
 
 		it { should render_template :index }

761c58d35dc61bfe16ba7ac028f13b97f40dff7d
Alexander Tkachuk <tksasha@ya.ru>
2014-06-11 13:10:30 +0400

    remove unused

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require qtip/jquery.qtip
 //
 //= require jqgrid/i18n/grid.locale-ru
-//= require jqgrid/jquery.jqGrid.min
+//= require jqgrid/jquery.jqGrid.src.js
 //
 //= require cubesviewer/cubesviewer
 //= require cubesviewer/cubesviewer.cache
